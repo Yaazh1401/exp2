44eb029a7e4875260becd4fe57bf1e1b5a3e9ab9
Yaazh1401 <yaazhinisekar7@gmail.com>
2025-03-17 11:57:13 +0530

    first commit

---
+++ expapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from expapp.models import cienima
+# Register your models here.
+admin.site.register(cienima)
---
+++ expapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class cienima(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    image = models.ImageField(upload_to='cienima/')
+    description = models.TextField()
+    def __str__(self):
+        return self.name
+    
